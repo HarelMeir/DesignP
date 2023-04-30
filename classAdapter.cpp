d255a2114111a0ab8c6b4e7dfb67ede337f4f8f0
Harel Meir <harelgo7@gmail.com>
2023-04-30 09:38:55 +0300

    first commit

---
+++ classAdapter.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class DogVoicePrinter {
+public:
+    void print() {
+        cout << "bark bark.." << endl;
+    }
+};
+
+class Target {
+public:
+    virtual void request()=0;
+};
+
+class Adapter :public Target, public DogVoicePrinter {
+public:
+    void request() override {
+        print();
+    }
+};
+
+/*int main() {
+    Target* target = new Adapter();
+    target->request();
+}*/
